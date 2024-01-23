f350dd1e601e5ea2a6596ddaa3d9f4d1602bee16
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2024-01-23 11:08:39 -0500

    Init React app and create picture component.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Picture from "./components/Picture";
+
+function App() {
+  return (
+    <main>
+      <h1>Where&apos;s Waldo</h1>
+      <Picture />
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/components/Picture.jsx
@@ -0,0 +1,10 @@
+import waldo from "../assets/waldo.jpeg";
+import "./Picture.css";
+
+export default function Picture() {
+  return (
+    <>
+      <img src={waldo} alt="" />
+    </>
+  );
+}
